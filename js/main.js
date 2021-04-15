59a4032cd77b9e4d0593dc8eb97faa2cb8036dca
thecaasantos <thecaasantos@gmail.com>
2021-04-15 17:25:52 -0300

    feat: Adicinando novas funções

--- js/main.js
+++ js/main.js
@@ -57,7 +57,69 @@ var frutas = [{                                 //Um array de dicionários
     cor:"Roxa"
 }]
 
-console.log(frutas[0].nome);                    //Retorna o item da 0 da lista frutas e seu atributo nome
+console.log(frutas[0].nome);                   //Retorna o item da 0 da lista frutas e seu atributo nome
 console.log(frutas[0].cor);                    //Retorna o item da 0 da lista frutas e seu atributo cor
-console.log(frutas[1].nome);                    //Retorna o item da 1 da lista frutas e seu atributo nome
+console.log(frutas[1].nome);                   //Retorna o item da 1 da lista frutas e seu atributo nome
 console.log(frutas[1].cor);                    //Retorna o item da 1 da lista frutas e seu atributo cor
+
+
+
+function soma(x, y){                            //Cria uma função  que pode ser chamada em seguida
+    return x + y;
+}
+
+console.log(soma(1,2));
+
+function validarIdade(idade){
+    var validar;
+    if (idade>=18){
+        validar = true;
+    }else{
+        validar = false;
+    }
+    return validar;
+}
+
+console.log(validarIdade(idade));
+
+//console.log(validar);                           //Neste caso a variavel validar funcina apenas do escopo da função definido por {}
+
+function botao(){                               //Função javascript
+    alert("Obrigado por clicar");
+}
+
+function obrigado(){                            // funcção que seleciona um elemento pelo id e injeta um texto dentro da tag HTML
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+}
+
+
+function redirecionar(){                        //função que redireciona para uma página espessifica
+    window.open("www.youtube.com");             // para abrir em nova aba
+    //window.location.href = "www.youtube.com.br";    //para abrir na mesma aba
+}
+
+function trocar(){                              //função que troca um texto de uma tag
+    document.getElementById("mouse").innerHTML = "Obrigado por passar o mouse";
+}
+
+
+function voltar(){                              //função que troca texto de uma tag
+    document.getElementById("mouse").innerHTML = "Passe o mouose aqui";
+
+}
+
+function foo(elemento){                         //Função que troca texto sem utilizar o getElementById
+    elemento.innerHTML = "Troca realizada com sucesso";
+}
+
+function boo(elemento){
+    elemento.innerHTML = "Passe o mouse para trocar";
+}
+
+function load(){
+    window.open("www.youtube.com.br")
+}
+
+function funChange(elemento){
+    console.log(elemento.value);
+}
